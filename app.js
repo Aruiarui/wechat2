41565a12e16b6221f313229c6ea8b2832267347e
yueban <yueban@qq.com>
2019-03-20 17:59:27 +0800

    初始化文件

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  
+});
+app.listen(3000,err=>{
+  if(!err) console.log('路由连接成功');
+  else console.log('服务器连接失败');
+});
